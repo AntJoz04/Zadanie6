538b7e41eab711512f83a4777513666c3f0af397
s34113 <s34113@pjwstk.edu.pl>
2026-04-16 17:38:14 +0200

    dodanie reservation controller

---
+++ Zadanie6/Controllers/ReservationController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zadanie6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+    }
+}
